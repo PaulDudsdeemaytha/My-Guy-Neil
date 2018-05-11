336592213fae4c1e7f4b6a2d1b8aeb231dc1c524
Ryan Cs. Ku <ryancsku@gmail.com>
2018-05-11 10:05:56 -0700

    Rename folder structure and integrate additional folder "app" and files.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,11 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import React, { Component } from "react";
+import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
