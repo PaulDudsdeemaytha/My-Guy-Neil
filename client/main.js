b7f3010f46942c03bf87a087e20d751fcb3c9caf
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2018-05-11 12:20:21 -0700

    added routes files

--- client/main.js
+++ client/main.js
@@ -1,27 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
 
 import React, { Component } from "react";
 import { render } from "react-dom";
 import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
+import styles from "./main.css";
 
-import './main.html';
+import "./main.html";
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  render(<HelloWorld />, document.getElementById("app"));
 });
---
+++ import/ui/startup/routes/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch, Link, Redirect } from "react-router-dom";
+//importing items for routes
+import Login from "../containers/Login";
+
+const Routes = () => (
+  <Switch>
+    <Route path="/Login" component={Login} />
+  </Switch>
+);
+
+export default Routes;
