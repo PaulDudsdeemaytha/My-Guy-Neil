ee9fd31256d6b78ec18e0f906d4a69d5fad83d9d
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2018-05-15 14:34:00 -0700

    added login template for login page

--- import/ui/containers/login/LoginContainer.js
+++ import/ui/containers/login/LoginContainer.js
@@ -2,7 +2,39 @@ import React, { Component } from "react";
 
 class LoginContainer extends Component {
   render() {
-    return <div>"Hello This is the login page"</div>;
+    return (
+      <div>
+        <h2 class="login-form">Account Login</h2>
+        <form>
+          <label for="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            class="form-control"
+            placeholder="Email Address"
+          />
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+            id="login-btn"
+            class="btn btn-lg btn-primary btn-block"
+          >
+            Log In
+          </button>
+          <p>
+            No Account? <a href="/signup">Create Account</a>
+          </p>
+        </form>
+      </div>
+    );
   }
 }
 
