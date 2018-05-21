810b394277266c5f846432f38ff7538cdd896b9b
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2018-05-21 12:05:29 -0700

    added chat features

--- imports/ui/components/app/style.js
+++ imports/ui/components/app/style.js
@@ -1,4 +1,4 @@
-import { renderRoutes } from '../../../startup/routes/index.js';
+import { renderRoutes } from "../../../startup/routes/index.js";
 
 const style = {
   home: {
@@ -7,72 +7,77 @@ const style = {
     // // justifyContent: 'center',
     // // flexDirection: 'column',
     background:
-      'center no-repeat url(https://source.unsplash.com/2560x1600/?galaxy)',
-    height: '100vh',
-    width: '100vw',
-    padding: '0',
-    border: '0',
-    margin: '0'
+      "center no-repeat url(https://source.unsplash.com/2560x1600/?galaxy)",
+    height: "100vh",
+    width: "100vw",
+    padding: "0",
+    border: "0",
+    margin: "0"
   },
 
   header: {
-    alignSelf: 'center',
-    color: '#C92C30',
-    fontFamily: 'Impact',
-    fontSize: '5.5rem',
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-    textShadow: '2.5px 2.5px #ffffff',
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100vw',
-    justifyContent: 'space-between',
-    top: '0',
-    right: '0',
-    padding: '1rem 0 0 1rem',
-    border: '0',
-    margin: '0'
+    alignSelf: "center",
+    color: "#C92C30",
+    fontFamily: "Impact",
+    fontSize: "5.5rem",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    textShadow: "2.5px 2.5px #ffffff",
+    display: "flex",
+    flexDirection: "column",
+    width: "100vw",
+    justifyContent: "space-between",
+    top: "0",
+    right: "0",
+    padding: "0",
+    border: "0",
+    margin: "0",
+    marginLeft: "0",
+    textAlign: "center"
   },
 
   AccountsUIWrapper: {
-    color: '#C92C30',
-    fontFamily: 'Impact',
-    fontSize: '1.5rem',
-    fontStyle: 'italic',
-    textShadow: '0.75px 0.75px #ffffff',
-    fontWeight: 'bold',
-    padding: '1rem 0 0 0.75rem',
-    border: '0',
-    margin: '0',
-    width: '250px'
+    color: "#C92C30",
+    fontFamily: "Impact",
+    fontSize: "1.5rem",
+    fontStyle: "italic",
+    textShadow: "0.75px 0.75px #ffffff",
+    fontWeight: "bold",
+    padding: "1rem 0 0 0.75rem",
+    border: "0",
+    margin: "0",
+    width: "100%",
+    textAlign: "center",
+    textDecoration: "none"
   },
 
   body: {
-    alignSelf: 'center',
-    color: '#ffffff',
-    fontFamily: 'Helvetica',
-    fontSize: '7.5rem',
-    fontWeight: 'bold',
+    alignSelf: "center",
+    color: "#ffffff",
+    fontFamily: "Helvetica",
+    fontSize: "7.5rem",
+    fontWeight: "bold",
     // fontStyle: 'italic',
-    textShadow: '3.75px 3.75px #C92C30',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '12.5%'
+    textShadow: "3.75px 3.75px #C92C30",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: "100px",
+    paddingBottom: "100px"
   },
 
   footer: {
-    color: '#ffffff',
-    textShadow: '0.75px 0.75px #C92C30',
-    display: 'flex',
-    flexDirection: 'row',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#ffffff',
-    fontFamily: 'Helvetica',
-    fontSize: '1.5rem',
-    fontWeight: 'bold'
+    color: "#ffffff",
+    textShadow: "0.75px 0.75px #C92C30",
+    display: "flex",
+    flexDirection: "row",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#ffffff",
+    fontFamily: "Helvetica",
+    fontSize: "1.5rem",
+    fontWeight: "bold"
   }
 };
 
--- imports/ui/containers/Login/Login.js
+++ imports/ui/containers/Login/Login.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import AccountsUIWrapper from "../../AccountsUIWrapper.js";
-
-// import { renderRoutes } from "../../startup/routes";
+import MessageList from "./MessageList";
+import Message from "./Message";
 
 class Login extends Component {
-  // onSubmit(e) {
-  //   e.preventDefault();
-  //   var ele = $(e.target);
-  //   //get our values input
-  //   var email = ele.find("#email").val();
-  //   var password = ele.find("#password").val();
-  //   var confirmPassword = ele.find("#confirmPassword").val();
-  //   //Log the user in
-  //   Meteor.loginWithPassword(email, password, er => {
-  //     if (er) {
-  //       alert(er.reason);
-  //     } else {
-  //       return <Redirect to="/" />;
-  //     }
-  //   });
-  // }
   render() {
     return (
       <div>
         <h2>Login</h2>
-        <AccountsUIWrapper />
-        {/* <form onSubmit={this.onSubmit} className="login">
+        <MessageList />
+      </div>
+    );
+  }
+}
+
+export default Login;
+
+// onSubmit(e) {
+//   e.preventDefault();
+//   var ele = $(e.target);
+//   //get our values input
+//   var email = ele.find("#email").val();
+//   var password = ele.find("#password").val();
+//   var confirmPassword = ele.find("#confirmPassword").val();
+//   //Log the user in
+//   Meteor.loginWithPassword(email, password, er => {
+//     if (er) {
+//       alert(er.reason);
+//     } else {
+//       return <Redirect to="/" />;
+//     }
+//   });
+// }        <AccountsUIWrapper />
+/* <form onSubmit={this.onSubmit} className="login">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" />
           <label htmlFor="password">Password</label>
@@ -35,10 +42,4 @@ class Login extends Component {
           <input id="confirmPassword" type="password" name="confirmPassword" />
           <input type="submit" value="Login" />
         </form>
-        <input type="button" value="Register now!" /> */}
-      </div>
-    );
-  }
-}
-
-export default Login;
+        <input type="button" value="Register now!" /> */
